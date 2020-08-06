562b3a73b014aea714c2123ec591dc125c950408
sturmlichtz <roshanrbhoir@gmail.com>
2020-08-06 13:36:35 +0530

    add feature to display all address

---
+++ Set_Person.java
@@ -0,0 +1,14 @@
+public class Set_Person {
+	String firstName;
+	String lastName;
+	String address;
+	String city;
+	String state;
+	int zip;
+	int phone;
+	public Set_Person(String firstName,  String lastName,  String address,  int zip) {
+		this.firstName=firstName; 
+		this.lastName=lastName;
+		this.address=address;
+		this.zip=zip;
+	}
